d7eb66b885098305206e9da9eb009459e7f9d711
LuizFXavier <zap422238@gmail.com>
2024-10-28 00:51:30 -0400

    feat: Utilização das novas funções de configuração

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Grafico.hpp"
+// #include "Grafico.hpp"
 #include "PSO.hpp"
 using namespace std;
 
@@ -10,10 +10,16 @@ int main(int argc, char * argv[]){
     }
     
     PSO pso(argv[1]);
-
-    pso.executar();
     
+    pso.set_properties(argv[2]);
+
+    cout << pso.instance_name << ":"<<endl;
+    pso.executar();
+    Particle best = pso.get_best();
+
+    pso.apresentar(best);
+
     cout << pso.get_best().best_dist << endl;
-    Grafico::apresentar(pso.cidades, pso.get_solution(pso.get_best()));
+    //Grafico::apresentar(pso.cidades, pso.get_solution(pso.get_best()));
     return EXIT_SUCCESS;
 }
