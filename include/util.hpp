72147a14d3bfb744bf42d47f85a6b56a8d1bb7ba
LuizFXavier <zap422238@gmail.com>
2025-04-03 15:23:26 -0400

    fix: Cálculo de distância incorreto

--- include/util.hpp
+++ include/util.hpp
@@ -7,7 +7,7 @@ namespace util
 {
     inline double
     calcula_distancia(Cidade &a, Cidade &b){ 
-        return sqrt(pow(a.x - b.x, 2) + pow(a.y - a.y, 2));
+        return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
     }
 
     unsigned time_seed = std::chrono::system_clock::now().time_since_epoch().count();
