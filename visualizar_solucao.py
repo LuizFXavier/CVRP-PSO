f945c6a6d80c24335d54a73e3f8de7ee822b7526
LuizFXavier <zap422238@gmail.com>
2025-04-22 23:56:54 -0400

    feat: Pintura dos nós e arestas condizente com a do cvrplib

--- visualizar_solucao.py
+++ visualizar_solucao.py
@@ -54,23 +54,44 @@ print("custo:" ,custo)
 
 rota = rota.split('-')
 
-cores = ['blue', 'red', 'green', 'cyan', 'navy', 'darkred', 'crimson', 'lime', 'springgreen', 'pink', 'peru', 'slateblue', 'dimgray', 'hotpink', 'aquamarine', 'olive', 'fuchsia', 'darkseagrean']
+cores = ['#3366cc', 
+         '#dc3912', 
+         '#ff9900', 
+         '#109618', 
+         '#990099', 
+         '#0099c6', 
+         '#dd4477', 
+         '#66aa00', 
+         '#b82e2e', 
+         '#316395', 
+         '#994499', 
+         '#22aa99', 
+         '#aaaa11', 
+         '#6633cc', 
+         '#e67300', 
+         'olive', 'fuchsia', 'darkseagrean']
 
 # cor_atual = f"#{randint(0, 255):02x}{randint(0, 255):02x}{randint(0, 255):02x}"
 
 k = 0
-
+cores_nos = {}
 for i in range(len(rota) - 1):
     G.add_edge(int(rota[i]) +1, int(rota[i+1]) +1, color = cores[k% len(cores)])
-
+    cores_nos[int(rota[i])] = cores[k% len(cores)]
     if(int(rota[i+1]) == 0):
         # cor_atual = f"#{randint(0, 255):02x}{randint(0, 255):02x}{randint(0, 255):02x}"
         k += 1
-    
+
+node_colors = [None] * len(cores_nos)
+
+for chave, valor in cores_nos.items():
+    node_colors[chave] = valor
+
+node_colors[0] = 'black'
 
 edge_colors = [G.edges[edge]['color'] for edge in G.edges()] 
 
-nx.draw(G, coordenadas, node_color='rebeccapurple', edge_color=edge_colors, node_size=10)
+nx.draw(G, coordenadas, node_color=node_colors, edge_color=edge_colors, node_size=10)
 
 plt.title(nome_instancia)
 
