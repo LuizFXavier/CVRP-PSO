4fb32c9231835ea61b61badfb159283571f63b2e
LuizFXavier <zap422238@gmail.com>
2026-07-04 16:57:12 -0400

    test: Teste para o comportamento do swap*

--- src_new/libcvrp-engine/libcvrp/engine/local-search.test.cpp
+++ src_new/libcvrp-engine/libcvrp/engine/local-search.test.cpp
@@ -8,11 +8,13 @@
 #include <libcvrp/core/Route.hpp>
 
 void test_two_opt();
+void test_swap_star();
 
 int
 main()
 {
   test_two_opt();
+  test_swap_star();
   return 0;
 }
 
@@ -49,5 +51,57 @@ test_two_opt()
 
     if (comparison[i] != routes[0].path[i])
       throw std::runtime_error(std::format("Error: two-opt did not produce correct output! Expected {}, but got {}", comparison, routes[0].path));
+  }
+}
+
+void 
+test_swap_star(){
+  cvrp::Instance inst;
+  inst.dimension = 4; // 1 Depósito e 3 Clientes
+  inst.capacity = 10;
+  
+  // Depósito A na origem (0,0)
+  inst.clients.push_back({0.0f, 0.0f, 0});
+
+  // Cliente B/1
+  inst.clients.push_back({0.0f, 3.0f, 1});
+
+  // Cliente C/2
+  inst.clients.push_back({3.0f, 1.0f, 1});
+
+  // Cliente D/3
+  inst.clients.push_back({3.0f, 0.0f, 1});
+
+  // Cliente E/4
+  inst.clients.push_back({1.0f, 3.0f, 1});
+
+  // Cliente F/5
+  inst.clients.push_back({2.0f, 1.0f, 1});
+
+  // Cliente G/6
+  inst.clients.push_back({1.0f, 2.0f, 1});
+
+  cvrp::Route route1, route2;
+
+  route1.path = {0, 1, 4, 5, 0};
+  route2.path = {0, 6, 2, 3, 0};
+
+  std::vector<cvrp::Route> routes = {route1, route2};
+
+  cvrp::local_search::apply_swap_star(routes, inst);
+
+  std::vector<int> comparison1 = {0, 1, 4, 6, 0};
+  std::vector<int> comparison2 = {0, 5, 2, 3, 0};
+
+  for (int i = 0; i < comparison1.size(); ++i){
+
+    if (comparison1[i] != routes[0].path[i])
+      throw std::runtime_error(std::format("Error: Swap star failed on first route! Expected {}, but got {}", comparison1, routes[0].path));
+  }
+
+  for (int i = 0; i < comparison2.size(); ++i){
+
+    if (comparison2[i] != routes[1].path[i])
+      throw std::runtime_error(std::format("Error: Swap star failed on second route! Expected {}, but got {}", comparison2, routes[1].path));
   }
 }
